96ea0ab482e7e6cc9974fe1dc7de0c126c126e07
hansel-no-kioku <hansel-no-kioku@users.noreply.github.com>
2019-12-07 08:57:00 +0900

    レスポンスのヘッダーに Content-type と Content-length 追加

--- src/Main.purs
+++ src/Main.purs
@@ -5,11 +5,13 @@ module Main
 import Prelude
 
 import Control.Monad.Indexed.Qualified as Ix
-import Data.Array (elem, null)
+import Data.Array (null)
 import Data.Either (Either(..), either)
 import Data.HTTP.Method (Method(..))
 import Data.Lazy (force)
 import Data.Maybe (Maybe(..))
+import Data.MediaType.Common (applicationJSON, applicationJavascript)
+import Data.Newtype (unwrap)
 import Data.Tuple.Unicode (type (×), (×))
 import Effect (Effect)
 import Effect.Aff (Aff, message, try)
@@ -24,6 +26,7 @@ import Hyper.Node.Server (HttpRequest, HttpResponse, defaultOptionsWithLogging, 
 import Hyper.Request (RequestData, getRequestData, readBody)
 import Hyper.Response (ResponseEnded, StatusLineOpen, headers, respond, writeHeader, writeStatus)
 import Hyper.Status (Status, status, statusNotFound, statusOK)
+import Node.Encoding (Encoding(..), byteLength)
 import Node.FsExtra (outputFileSync, pathExists, readFile, readFileSync, removeSync)
 import Node.Path (concat)
 import Prelude.Unicode ((∘))
@@ -87,7 +90,7 @@ compile ∷ String → String → Aff ResponseData
 compile src backend = liftEffect do
   outputFileSync (concat ["backend", "src", "Main.purs"]) src
   compileErrors ← P.compile backend
-  result ← if null compileErrors.errors
+  body ← if null compileErrors.errors
     then do
       js ← readFileSync $ concat ["backend", backend, "output", "Main", "index.js"]
       pure $ writeJSON {warnings: compileErrors.warnings, js}
@@ -97,14 +100,20 @@ compile src backend = liftEffect do
   removeSync $ concat ["backend", backend, "output", "Main"]
   removeSync $ concat ["backend", "src"]
 
-  pure {status: statusOK, headers: [], body: result}
+  pure { status: statusOK
+       , headers: [ "Content-Type" × unwrap applicationJSON
+                  ]
+       , body
+       }
 
 
 bundle ∷ String → Aff ResponseData
 bundle backend = do
   body ← readFile $ concat ["backend", backend, "bundle.js"]
   pure { status: statusOK
-       , headers: [ "Cache-Control" × "max-age=600" ]
+       , headers: [ "Cache-Control" × "max-age=600"
+                  , "Content-Type" × unwrap applicationJavascript
+                  ]
        , body
        }
 
@@ -114,7 +123,9 @@ respond'
   → ResponseData
   → Middleware Aff (Conn HttpRequest (HttpResponse StatusLineOpen) {}) (Conn HttpRequest (HttpResponse ResponseEnded) {}) Unit
 respond' cors res = Ix.do
+  let contentLength = byteLength res.body UTF8
   writeStatus res.status
+  when (contentLength > 0) $ writeHeader $ "Content-Length" × show contentLength
   when cors $ writeHeader $ "Access-Control-Allow-Origin" × "*"
   headers res.headers
   respond res.body
